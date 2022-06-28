3d728ed8b747b2d41dd79e4fbe0ed5fe5964a33f
Lyric <5h3ll3x@gmail.com>
2022-06-27 23:10:42 -0700

    fix: minor

--- src/constants.ts
+++ src/constants.ts
@@ -27,7 +27,7 @@ export const CLIENT_ID_DEV = "8e9e520c-b002-4bfe-b989-3f18d8d6e273";
 export const CLIENT_ID_PROD = "0690b112-e607-4493-b1f0-7a39cf14bad6";
 
 export const CLIENT_ID =
-  process.env.CLIENT_ID || (isProduct ? CLIENT_ID_DEV : CLIENT_ID_PROD);
+  process.env.CLIENT_ID || (isProduct ? CLIENT_ID_PROD : CLIENT_ID_DEV);
 
 export const CONFIG = {
   MIXIN_CLIENT_ID: CLIENT_ID,
